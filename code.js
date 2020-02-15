93aec8b2b542e74dfbf9077bbf96ec63285e4bdb
Serge Zaitsev <zaitsev.serge@gmail.com>
2020-02-15 20:17:48 +0000

    use point interface everywhere

--- code.js
+++ code.js
@@ -65,166 +65,158 @@ function clone(point) {
 function vec2(x, y) {
     return [x, y];
 }
-function createBezierBuilder(opt) {
-    opt = opt || {};
-    var RECURSION_LIMIT = typeof opt.recursion === 'number' ? opt.recursion : 8;
-    var FLT_EPSILON = typeof opt.epsilon === 'number' ? opt.epsilon : 1.19209290e-7;
-    var PATH_DISTANCE_EPSILON = typeof opt.pathEpsilon === 'number' ? opt.pathEpsilon : 1.0;
-    var curve_angle_tolerance_epsilon = typeof opt.angleEpsilon === 'number' ? opt.angleEpsilon : 0.01;
-    var m_angle_tolerance = opt.angleTolerance || 0;
-    var m_cusp_limit = opt.cuspLimit || 0;
-    return function bezierCurve(start, c1, c2, end, scale, points) {
-        if (!points)
-            points = [];
-        scale = typeof scale === 'number' ? scale : 1.0;
-        var distanceTolerance = PATH_DISTANCE_EPSILON / scale;
-        distanceTolerance *= distanceTolerance;
-        begin(start, c1, c2, end, points, distanceTolerance);
-        return points;
-    };
-    ////// Based on:
-    ////// https://github.com/pelson/antigrain/blob/master/agg-2.4/src/agg_curves.cpp
-    function begin(start, c1, c2, end, points, distanceTolerance) {
-        points.push(clone(start));
-        var x1 = start[0], y1 = start[1], x2 = c1[0], y2 = c1[1], x3 = c2[0], y3 = c2[1], x4 = end[0], y4 = end[1];
-        recursive(x1, y1, x2, y2, x3, y3, x4, y4, points, distanceTolerance, 0);
-        points.push(clone(end));
+const RECURSION_LIMIT = 8;
+const FLT_EPSILON = 1.19209290e-7;
+const curve_angle_tolerance_epsilon = 0.01;
+const m_angle_tolerance = 0;
+const m_cusp_limit = 0;
+function renderBezier(start, c1, c2, end) {
+    const points = [];
+    points.push(start);
+    const { x: x1, y: y1 } = start;
+    const { x: x2, y: y2 } = c1;
+    const { x: x3, y: y3 } = c2;
+    const { x: x4, y: y4 } = end;
+    recursive(x1, y1, x2, y2, x3, y3, x4, y4, points, 1, 0);
+    points.push(end);
+    return points;
+}
+function recursive(x1, y1, x2, y2, x3, y3, x4, y4, points, distanceTolerance, level) {
+    if (level > RECURSION_LIMIT) {
+        return;
     }
-    function recursive(x1, y1, x2, y2, x3, y3, x4, y4, points, distanceTolerance, level) {
-        if (level > RECURSION_LIMIT)
-            return;
-        var pi = Math.PI;
-        // Calculate all the mid-points of the line segments
-        //----------------------
-        var x12 = (x1 + x2) / 2;
-        var y12 = (y1 + y2) / 2;
-        var x23 = (x2 + x3) / 2;
-        var y23 = (y2 + y3) / 2;
-        var x34 = (x3 + x4) / 2;
-        var y34 = (y3 + y4) / 2;
-        var x123 = (x12 + x23) / 2;
-        var y123 = (y12 + y23) / 2;
-        var x234 = (x23 + x34) / 2;
-        var y234 = (y23 + y34) / 2;
-        var x1234 = (x123 + x234) / 2;
-        var y1234 = (y123 + y234) / 2;
-        if (level > 0) { // Enforce subdivision first time
-            // Try to approximate the full cubic curve by a single straight line
-            //------------------
-            var dx = x4 - x1;
-            var dy = y4 - y1;
-            var d2 = Math.abs((x2 - x4) * dy - (y2 - y4) * dx);
-            var d3 = Math.abs((x3 - x4) * dy - (y3 - y4) * dx);
-            var da1, da2;
-            if (d2 > FLT_EPSILON && d3 > FLT_EPSILON) {
-                // Regular care
-                //-----------------
-                if ((d2 + d3) * (d2 + d3) <= distanceTolerance * (dx * dx + dy * dy)) {
-                    // If the curvature doesn't exceed the distanceTolerance value
-                    // we tend to finish subdivisions.
+    const pi = Math.PI;
+    // Calculate all the mid-points of the line segments
+    //----------------------
+    const x12 = (x1 + x2) / 2;
+    const y12 = (y1 + y2) / 2;
+    const x23 = (x2 + x3) / 2;
+    const y23 = (y2 + y3) / 2;
+    const x34 = (x3 + x4) / 2;
+    const y34 = (y3 + y4) / 2;
+    const x123 = (x12 + x23) / 2;
+    const y123 = (y12 + y23) / 2;
+    const x234 = (x23 + x34) / 2;
+    const y234 = (y23 + y34) / 2;
+    const x1234 = (x123 + x234) / 2;
+    const y1234 = (y123 + y234) / 2;
+    if (level > 0) { // Enforce subdivision first time
+        // Try to approximate the full cubic curve by a single straight line
+        //------------------
+        let dx = x4 - x1;
+        let dy = y4 - y1;
+        let d2 = Math.abs((x2 - x4) * dy - (y2 - y4) * dx);
+        let d3 = Math.abs((x3 - x4) * dy - (y3 - y4) * dx);
+        let da1, da2;
+        if (d2 > FLT_EPSILON && d3 > FLT_EPSILON) {
+            // Regular care
+            //-----------------
+            if ((d2 + d3) * (d2 + d3) <= distanceTolerance * (dx * dx + dy * dy)) {
+                // If the curvature doesn't exceed the distanceTolerance value
+                // we tend to finish subdivisions.
+                //----------------------
+                if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+                    points.push({ x: x1234, y: y1234 });
+                    return;
+                }
+                // Angle & Cusp Condition
+                //----------------------
+                let a23 = Math.atan2(y3 - y2, x3 - x2);
+                da1 = Math.abs(a23 - Math.atan2(y2 - y1, x2 - x1));
+                da2 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - a23);
+                if (da1 >= pi)
+                    da1 = 2 * pi - da1;
+                if (da2 >= pi)
+                    da2 = 2 * pi - da2;
+                if (da1 + da2 < m_angle_tolerance) {
+                    // Finally we can stop the recursion
                     //----------------------
-                    if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
-                        points.push(vec2(x1234, y1234));
+                    points.push({ x: x1234, y: y1234 });
+                    return;
+                }
+                if (m_cusp_limit !== 0.0) {
+                    if (da1 > m_cusp_limit) {
+                        points.push({ x: x2, y: y2 });
                         return;
                     }
-                    // Angle & Cusp Condition
+                    if (da2 > m_cusp_limit) {
+                        points.push({ x: x3, y: y3 });
+                        return;
+                    }
+                }
+            }
+        }
+        else {
+            if (d2 > FLT_EPSILON) {
+                // p1,p3,p4 are collinear, p2 is considerable
+                //----------------------
+                if (d2 * d2 <= distanceTolerance * (dx * dx + dy * dy)) {
+                    if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+                        points.push({ x: x1234, y: y1234 });
+                        return;
+                    }
+                    // Angle Condition
                     //----------------------
-                    var a23 = Math.atan2(y3 - y2, x3 - x2);
-                    da1 = Math.abs(a23 - Math.atan2(y2 - y1, x2 - x1));
-                    da2 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - a23);
+                    da1 = Math.abs(Math.atan2(y3 - y2, x3 - x2) - Math.atan2(y2 - y1, x2 - x1));
                     if (da1 >= pi)
                         da1 = 2 * pi - da1;
-                    if (da2 >= pi)
-                        da2 = 2 * pi - da2;
-                    if (da1 + da2 < m_angle_tolerance) {
-                        // Finally we can stop the recursion
-                        //----------------------
-                        points.push(vec2(x1234, y1234));
+                    if (da1 < m_angle_tolerance) {
+                        points.push({ x: x2, y: y2 });
+                        points.push({ x: x3, y: y3 });
                         return;
                     }
                     if (m_cusp_limit !== 0.0) {
                         if (da1 > m_cusp_limit) {
-                            points.push(vec2(x2, y2));
+                            points.push({ x: x2, y: y2 });
                             return;
                         }
-                        if (da2 > m_cusp_limit) {
-                            points.push(vec2(x3, y3));
+                    }
+                }
+            }
+            else if (d3 > FLT_EPSILON) {
+                // p1,p2,p4 are collinear, p3 is considerable
+                //----------------------
+                if (d3 * d3 <= distanceTolerance * (dx * dx + dy * dy)) {
+                    if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+                        points.push({ x: x1234, y: y1234 });
+                        return;
+                    }
+                    // Angle Condition
+                    //----------------------
+                    da1 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - Math.atan2(y3 - y2, x3 - x2));
+                    if (da1 >= pi)
+                        da1 = 2 * pi - da1;
+                    if (da1 < m_angle_tolerance) {
+                        points.push({ x: x2, y: y2 });
+                        points.push({ x: x3, y: y3 });
+                        return;
+                    }
+                    if (m_cusp_limit !== 0.0) {
+                        if (da1 > m_cusp_limit) {
+                            points.push({ x: x3, y: y3 });
                             return;
                         }
                     }
                 }
             }
             else {
-                if (d2 > FLT_EPSILON) {
-                    // p1,p3,p4 are collinear, p2 is considerable
-                    //----------------------
-                    if (d2 * d2 <= distanceTolerance * (dx * dx + dy * dy)) {
-                        if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
-                            points.push(vec2(x1234, y1234));
-                            return;
-                        }
-                        // Angle Condition
-                        //----------------------
-                        da1 = Math.abs(Math.atan2(y3 - y2, x3 - x2) - Math.atan2(y2 - y1, x2 - x1));
-                        if (da1 >= pi)
-                            da1 = 2 * pi - da1;
-                        if (da1 < m_angle_tolerance) {
-                            points.push(vec2(x2, y2));
-                            points.push(vec2(x3, y3));
-                            return;
-                        }
-                        if (m_cusp_limit !== 0.0) {
-                            if (da1 > m_cusp_limit) {
-                                points.push(vec2(x2, y2));
-                                return;
-                            }
-                        }
-                    }
-                }
-                else if (d3 > FLT_EPSILON) {
-                    // p1,p2,p4 are collinear, p3 is considerable
-                    //----------------------
-                    if (d3 * d3 <= distanceTolerance * (dx * dx + dy * dy)) {
-                        if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
-                            points.push(vec2(x1234, y1234));
-                            return;
-                        }
-                        // Angle Condition
-                        //----------------------
-                        da1 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - Math.atan2(y3 - y2, x3 - x2));
-                        if (da1 >= pi)
-                            da1 = 2 * pi - da1;
-                        if (da1 < m_angle_tolerance) {
-                            points.push(vec2(x2, y2));
-                            points.push(vec2(x3, y3));
-                            return;
-                        }
-                        if (m_cusp_limit !== 0.0) {
-                            if (da1 > m_cusp_limit) {
-                                points.push(vec2(x3, y3));
-                                return;
-                            }
-                        }
-                    }
-                }
-                else {
-                    // Collinear case
-                    //-----------------
-                    dx = x1234 - (x1 + x4) / 2;
-                    dy = y1234 - (y1 + y4) / 2;
-                    if (dx * dx + dy * dy <= distanceTolerance) {
-                        points.push(vec2(x1234, y1234));
-                        return;
-                    }
+                // Collinear case
+                //-----------------
+                dx = x1234 - (x1 + x4) / 2;
+                dy = y1234 - (y1 + y4) / 2;
+                if (dx * dx + dy * dy <= distanceTolerance) {
+                    points.push({ x: x1234, y: y1234 });
+                    return;
                 }
             }
         }
-        // Continue subdivision
-        //----------------------
-        recursive(x1, y1, x12, y12, x123, y123, x1234, y1234, points, distanceTolerance, level + 1);
-        recursive(x1234, y1234, x234, y234, x34, y34, x4, y4, points, distanceTolerance, level + 1);
     }
+    // Continue subdivision
+    //----------------------
+    recursive(x1, y1, x12, y12, x123, y123, x1234, y1234, points, distanceTolerance, level + 1);
+    recursive(x1234, y1234, x234, y234, x34, y34, x4, y4, points, distanceTolerance, level + 1);
 }
+// Converts array of points to a smooth SVG path data like "M x y C sx sy ex ey px py C ..."
 function svgPathData(points) {
     const line = (a, b) => {
         const dx = b.x - a.x;
@@ -258,12 +250,10 @@ figma.currentPage.selection.filter(n => n.type === 'VECTOR').forEach(node => {
     const v = node;
     let allPoints = [];
     v.vectorNetwork.segments.forEach(segment => {
-        const v1 = v.vectorNetwork.vertices[segment.start];
-        const v2 = v.vectorNetwork.vertices[segment.end];
-        const start = [v1.x, v1.y];
-        const c1 = [v1.x, v1.y];
-        const c2 = [v2.x, v2.y];
-        const end = [v2.x, v2.y];
+        const start = v.vectorNetwork.vertices[segment.start];
+        const end = v.vectorNetwork.vertices[segment.end];
+        const c1 = { x: start.x, y: start.y };
+        const c2 = { x: end.x, y: end.y };
         if (segment.tangentStart) {
             c1[0] = c1[0] + segment.tangentStart.x;
             c1[1] = c1[1] + segment.tangentStart.y;
@@ -272,12 +262,10 @@ figma.currentPage.selection.filter(n => n.type === 'VECTOR').forEach(node => {
             c2[0] = c2[0] + segment.tangentEnd.x;
             c2[1] = c2[1] + segment.tangentEnd.y;
         }
-        const points = [];
-        const render = createBezierBuilder({});
-        render(start, c1, c2, end, 1.0, points);
+        const points = renderBezier(start, c1, c2, end);
         allPoints = allPoints.concat(points);
     });
-    allPoints = simplify(allPoints.map(p => ({ x: p[0], y: p[1] })), 3);
+    allPoints = simplify(allPoints, 2);
     const path = {
         windingRule: 'NONE',
         data: svgPathData(allPoints),
